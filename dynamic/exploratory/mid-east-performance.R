09a416e6cdfc5bf3cc58d51d5fa950e37e2d1912
Noah Peart <noah.v.peart@gmail.com>
2014-02-26 18:32:36 -0500

    Batch Update

---
+++ dynamic/exploratory/mid-east-performance.R
@@ -0,0 +1,66 @@
+## Compare plots in mid east that vary drastically in changes in ht / canht,
+## some are top performers others bottom performers in this category
+source("~/work/functions/functions-datatrans.R")
+library(ggplot2)
+library(grid)
+library(plyr)
+
+canhts <- read.csv("~/work/data/data/boles/canhts.csv")
+pp <- read.csv("~/work/data/data/dynamicallometry/moose-long-canopies.csv")
+
+## add survival indicator column, if tree died at any time during census
+##  it gets a 1
+pp$died <- ifelse(!is.na(pp$yrmort), 1, 0)
+
+## change pp time scale, pplot to factor, remove dead trees in 86/87
+pp[pp$time == 10,]$time <- 110
+pp$pplot <- as.factor(pp$pplot)
+pp <- pp[!pp$yrmort %in% c(1986, 1987),]
+
+## paths colored by survival
+tst <- pp[is.na(pp$eht) & pp$spec %in% c("ABBA", "PIRU") & pp$elevcl == "M" & pp$aspcl == "E",]
+tst <- droplevels(tst[!tst$pplot %in% c(1,2,3),])
+
+ggplot(tst, aes(dbh, ht/canht, group = id, col = died)) +
+    geom_path(arrow = arrow(), alpha = 0.5) + geom_point(alpha = 0.5) +
+    facet_wrap(~pplot, ncol = 2) + ggtitle("multiple species") + geom_hline(yintercept=1, lty = 2)
+    #xlim(0,5)
+
+
+## Look specifically at 8, 9, 11
+## Growth relative to canopy height
+tst <- droplevels(tst[tst$pplot %in% c(8,9,11),])
+tst[tst$time == 87,]$time <- 86
+tst <- subset(tst, !is.na(canht))
+
+ggplot(tst, aes(dbh, ht/canht, group = id, col = died)) +
+    geom_path(arrow = arrow(), alpha = 0.5) + geom_point(alpha = 0.5) +
+    facet_wrap(~pplot, ncol = 3) + ggtitle("Mid East Performers") +
+    geom_hline(yintercept=1, lty = 2)
+    #xlim(0,5)
+
+## Absolute growth
+ggplot(tst, aes(dbh, ht, group = id, col = died)) +
+    geom_path(arrow = arrow(), alpha = 0.5) + geom_point(alpha = 0.5) +
+    facet_wrap(~pplot, ncol = 3) + ggtitle("Mid East Performers") +
+    geom_hline(yintercept=1, lty = 2)
+    #xlim(0,5)
+
+## ht/dbh
+ggplot(tst, aes(dbh, ht/dbh, group = id, col = died)) +
+    geom_path(arrow = arrow(), alpha = 0.5) + geom_point(alpha = 0.5) +
+    facet_wrap(~pplot, ncol = 3) + ggtitle("Mid East Performers")
+
+## canht
+ggplot(tst, aes(time, canht, group = pplot, col = pplot)) +
+    geom_point() + geom_path(arrow = arrow()) + facet_wrap(~pplot)
+
+## density changes
+dens <- ddply(tst, .(pplot, time), .fun = function(x) {
+    x <- droplevels(x);
+    data.frame(dens = nrow(x[!is.na(x$dbh) | !is.na(x$ht),]))
+})
+
+ggplot(dens, aes(time, dens, group = pplot, col = pplot)) + geom_point() +
+    geom_path(arrow = arrow())
+
--- dynamic/exploratory/sgraphs-extremes.R
+++ dynamic/exploratory/sgraphs-extremes.R
@@ -11,3 +11,65 @@ pp <- read.csv("~/work/data/data/dynamicallometry/moose-long-canopies.csv")
 ## add survival indicator column, if tree died at any time during census
 ##  it gets a 1
 pp$died <- ifelse(!is.na(pp$yrmort), 1, 0)
+
+## transform to wide
+## time-varying columns
+ns <- c("stat", "decm", "dbh", "ht", "ba", "bv", "eht", "cht", "cpos", "dbhgrowth",
+        "htgrowth", "bagrowth", "bvgrowth", "priordbh", "priorht", "priorba",
+        "priorbv", "canht")
+
+## transform to wide
+wide <- reshape(pp, v.names = ns, direction = "wide")
+
+## Find extreme indviduals (across entires sampling interval)
+## those with large/small gains in ht/canht
+wide$p1 <-
+    ifelse(wide$pplot < 16 & wide$pplot > 3,
+        (wide$ht.10 - wide$canht.10) - (wide$ht.86 - wide$canht.86),
+           ifelse(wide$pplot >= 16,
+        (wide$ht.10 - wide$canht.10) - (wide$ht.87 - wide$canht.87), NA))
+
+qs <- quantile(wide$p1, na.rm=T)
+names(quantile(wide$p1, na.rm=T))
+## look at top performers
+top <- subset(wide, p1 > qs[["75%"]])
+table(top$spec)
+table(top$pplot)
+
+## bottom performers
+bottom <- subset(wide, p1 < qs[["25%"]])
+table(bottom$spec) ## BECO overrepresented
+table(bottom$pplot)
+
+## top perfomer plots: 9, 11, 18, 24, 27
+p11 <- subset(wide, pplot == 11)
+unique(p11$canht.86)
+unique(p11$canht.10)
+
+## bottom plots: 8, 13, 14, 20, 25
+
+
+## What is different between 8, 9, 11? (all ME)
+## 25, 27? (HW)
+
+## Absolute growth
+wide$totgrowth <-
+    ifelse(wide$pplot < 16 & wide$pplot > 3,
+        (wide$ht.10 - wide$ht.86),
+           ifelse(wide$pplot >= 16,
+        (wide$ht.10 -  wide$ht.87), NA))
+
+qs2 <- quantile(wide$totgrowth, na.rm=T)
+## look at top performers
+toptot <- subset(wide, totgrowth > qs2[["75%"]])
+table(toptot$spec)
+table(toptot$pplot)
+
+## bottom performers
+bottomtot <- subset(wide, totgrowth < qs2[["25%"]])
+table(bottomtot$spec) ## BECO overrepresented
+table(bottomtot$pplot)
+
+## plot 16
+p16 <- subset(wide, pplot ==16)
+quantile(p16$totgrowth, na.rm=T)
