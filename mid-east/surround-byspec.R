1fae7d564343fcc3d4ebfaecdd427a410eaa8c34
Noah Peart <noah.v.peart@gmail.com>
2014-04-18 16:30:29 -0400

    Batch Update

--- mid-east/surround-byspec.R
+++ mid-east/surround-byspec.R
@@ -34,7 +34,63 @@ grew <- which(!is.na(targets[,dep.var]) & targets$spec==spec & targets[,dep.var]
 abbas <- targets[grew,]
 
 # make neighbor matrices using square radius (i.e bqudx,bqudy)
-abba_mats<- make.neighbor.matrices(targets, neighbors, sr, ind.var=ind.var, bigger=TRUE)
+abba_mats<- mnm(abbas, neighbors, sr, ind.var=ind.var)
+
+## Assign Neighbor variables
+abbas$sumBa <- rowSums(abba_mats[["variable"]], na.rm=T)
+abbas$nDen <- abba_mats$number_neighbors
+
+## visualize
+table(abbas$pplot)
+table(abbas$pplot, abbas$time)
+
+## Growth vs. summed neighbor BA
+ggplot(abbas, aes(sumBa, bagrowth, group = id, col = factor(pplot))) + geom_path(arrow = arrow())
+
+## Growth vs. neighbor density
+ggplot(abbas, aes(nDen, bagrowth, group = id, col = factor(pplot))) +
+    geom_path(arrow = arrow()) + xlim(0, 20)
+
+## Neighborhood density by size of tree
+ggplot(abbas, aes(ba, nDen, col = pplot, group = factor(pplot))) + geom_point() +
+    geom_smooth(se = F) + facet_wrap(~time)
+
+## ht/dbh vs nDen
+library(ggplot2)
+ggplot(abbas, aes(nDen, ht/dbh)) +
+    geom_path(arrow = arrow(), alpha = 0.5, aes(group = id)) +
+    geom_point(aes(size = ba)) +
+    facet_wrap(~pplot, ncol = 3) + ggtitle("Mid East Performers") +
+    xlim(0,20) + ylim(0.3,1.5)
+
+ggplot(abbas, aes(nDen, ht/dbh)) +
+    geom_point(alpha = 0.5) + facet_wrap(~pplot) +
+    ggtitle("Mid East Performers") +
+    xlim(0,20) + ylim(0.3,1.5) + geom_smooth()
+
+## Surroundedness
+abbas$crowd <- percSurround(sr, abbas, abba_mats)
+
+## Visualize bagrowth vs crowdedness
+ggplot(abbas, aes(crowd, bagrowth)) + geom_point()
+ggplot(abbas, aes(crowd, bagrowth, group = id, col = factor(pplot))) + geom_path(arrow = arrow())
+
+## bagrowth vs crowdedness * sum neighbor BA
+ggplot(abbas, aes(sumBa*crowd, htgrowth/dbhgrowth, group = id, col = factor(pplot))) +
+    geom_path(arrow = arrow())
+
+## simple glm
+mod1 <- glm(bagrowth ~ priorba * (crowd + sumBa), data = abbas)
+mod2 <- glm(bagrowth ~ priorba * crowd, data = abbas)
+mod3 <- glm(bagrowth ~ priorba * sumBa, data = abbas)
+anova(mod1, mod2, mod3, test = "Chisq")
+plot(mod2)
+## plot
+tst <- abbas
+tst$pred <- predict(mod1)
+
+ggplot(tst, aes(priorba, bagrowth, color = crowd)) + geom_point() +
+    geom_point(aes(priorba, pred), pch = 3)
 
 ##################################################################################
 ##
@@ -48,16 +104,16 @@ grew <- which(!is.na(targets[,dep.var]) & targets$spec==spec & targets[,dep.var]
 pirus <- targets[grew,]
 
 # make neighbor matrices using square radius (i.e bqudx,bqudy)
-piru_mats<- make.neighbor.matrices(pirus, sr, ind.var=ind.var, bigger=TRUE)
+piru_mats<- make.neighbor.matrices(pirus, neighbors, sr, ind.var=ind.var)
 
 
 ##################################################################################
 ## Timing
-library(rbenchmark)
-benchmark(make.neighbor.matrices(targets, neighbors, sr, ind.var=ind.var, bigger=TRUE),
-          mnm(targets, neighbors, sr, ind.var=ind.var, bigger=TRUE),
-          columns = c("test", "replications", "elapsed", "relative"),
-          order = "relative", replications = 5)
+## library(rbenchmark)
+## benchmark(make.neighbor.matrices(targets, neighbors, sr, ind.var=ind.var, bigger=TRUE),
+##           mnm(targets, neighbors, sr, ind.var=ind.var, bigger=TRUE),
+##           columns = c("test", "replications", "elapsed", "relative"),
+##           order = "relative", replications = 5)
 
 ## 2                   mnm(targets, neighbors, sr, ind.var = ind.var, bigger = TRUE)
 ## 1 make.neighbor.matrices(targets, neighbors, sr, ind.var = ind.var, bigger = TRUE)
