99edb1c82a87b3aafe0fc063ac3d91537cb4b27f
Noah Peart <noah.v.peart@gmail.com>
2015-05-27 14:33:04 -0400

    Batch update

---
+++ gompertz/setup.R
@@ -0,0 +1,30 @@
+### setup.R --- 
+## Filename: setup.R
+## Description: Setup data for bootstrap.Rmd
+## Author: Noah Peart
+## Created: Wed May 27 12:59:58 2015 (-0400)
+## Last-Updated: Wed May 27 13:44:07 2015 (-0400)
+##           By: Noah Peart
+######################################################################
+
+source("~/work/allometry/gompertz/model.R")
+## source("~/work/allometry/gompertz/zach_setup.R")
+pp <- readRDS("~/work/allometry/gompertz/temp/pp.rds")
+res <- lm(ELEV ~ cht98, data=pp)
+pp$relev <- residuals(res)
+
+## Bootstrap for 98 data, sampling equally from quantiles of DBH98
+yr <- 98
+stat <- paste0("STAT", yr)
+dbh <- paste0("DBH", yr)
+ht <- paste0("HTTCR", yr)
+canht <- paste0("cht", yr)
+elev <- "relev"
+## dat <- pp[pp[,stat] == "ALIVE" & pp$SPEC == "ABBA" & !is.na(pp[,dbh]) &
+##              !is.na(pp[,ht]), ]
+## ps <- readRDS("~/work\\ecodatascripts\\vars\\heights\\gompertz\\full\\abba\\abba_98.rds")
+
+## Parameters from manuscript
+ps <- list(a=0.199, a1=0.000251, a2=0.00511, a3=-0.0000141, b=2.31, b1=0.00410, b2=0.92, b3=-0.000271,
+           sd=1)
+
--- gompertz/zach_setup.R
+++ gompertz/zach_setup.R
@@ -3,7 +3,7 @@
 ## Description: 
 ## Author: Noah Peart
 ## Created: Sat May 23 12:59:35 2015 (-0400)
-## Last-Updated: Sat May 23 16:43:31 2015 (-0400)
+## Last-Updated: Wed May 27 14:12:13 2015 (-0400)
 ##           By: Noah Peart
 ######################################################################
 pp <- read.csv("~/work/treedata/pp.csv")
@@ -16,6 +16,19 @@ for (i in 1:nrow(pp))
                               pp$BQUDX <= pp$BQUDX[i]+1 & pp$BQUDY <= pp$BQUDY[i]+1 & 
                                   pp$PPLOT == pp$PPLOT[i],
                           "HTTCR98"])
+
+## w/o target included
+pp$cht_notarget98 <- NA
+for (i in seq_along(cht_notarget)) {
+    hood <- pp[pp$BQUDX >= pp$BQUDX[i]-1 & 
+                   pp$BQUDY >= pp$BQUDY[i]-1 &
+                       pp$BQUDX <= pp$BQUDX[i]+1 & 
+                           pp$BQUDY <= pp$BQUDY[i]+1 & 
+                               pp$PPLOT == pp$PPLOT[i] &
+                                   pp$TAG != pp$TAG[i], ]
+    pp$cht_notarget98[i] <- ifelse (nrow(hood)>0, max(hood[,"HTTCR98"]), 0)
+}
+
 pp <- pp[!is.na(pp$DBH98) & pp$DBH98 > 0 & !is.na(pp$cht98) & pp$BQUDX > 1 & pp$BQUDX < 10 &
              pp$BQUDY > 1 & pp$BQUDY < 10, ]
 pp <- pp[pp$SPEC == "ABBA", ]
@@ -23,3 +36,5 @@ pp <- pp[pp$SPEC == "ABBA", ]
 ## Parameters from manuscript
 ps <- list(a=0.199, a1=0.000251, a2=0.00511, a3=-0.0000141, b=2.31, b1=0.00410, b2=0.92, b3=-0.000271,
            sd=1)
+
+saveRDS(pp, "~/work/allometry/gompertz/temp/pp.rds")
